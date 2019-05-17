3521abf51d8f331fb0a723ca7a64cff86f1fd2fe
romark99 <romark999@gmail.com>
2019-05-17 19:42:32 +0300

    lesson 3 completed.

--- convolutionalnn/src/main/java/com/brest/romark/convolutionalnn/MainActivity.java
+++ convolutionalnn/src/main/java/com/brest/romark/convolutionalnn/MainActivity.java
@@ -1,16 +1,55 @@
 package com.brest.romark.convolutionalnn;
 
+import android.graphics.SurfaceTexture;
 import android.os.Bundle;
+import android.view.TextureView;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextureView mTextureView;
+    private TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+            Toast.makeText(getApplicationContext(), "TextureView is available.", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceBundle) {
         super.onCreate(savedInstanceBundle);
         setContentView(R.layout.activity_main);
+
+        mTextureView = findViewById(R.id.textureView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mTextureView.isAvailable()) {
+
+        }
+        else {
+            mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+        }
     }
 
     @Override
